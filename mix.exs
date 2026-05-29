59d37101239d1fc37328eaa67c44160adc083591
Dmitri Don <fotkin@gmail.com>
2026-05-29 17:08:59 +0000

    Libs upgraded

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule PhoenixKitStaff.MixProject do
     [
       quality: ["format", "credo --strict", "dialyzer"],
       "quality.ci": ["format --check-formatted", "credo --strict", "dialyzer"],
-      precommit: ["compile", "quality"],
+      precommit: ["compile --force --warnings-as-errors", "deps.unlock --check-unused", "quality.ci"],
       "test.setup": [
         "ecto.create --quiet -r PhoenixKitStaff.Test.Repo"
       ],
